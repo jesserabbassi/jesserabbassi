7893ed0df104ef1b6ab0eb3155489c736b6746cc
Jesser Abbassi <jesserabbassi@gmail.com>
2026-06-05 19:38:42 +0100

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
-import "globals.css";
+import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { profile } from "@/lib/data";
 
